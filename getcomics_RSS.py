fc7be9b5a60bac96b559a3712a6144a24ec70aca
Mireille Gregoire <mireille.gregoire@phoenix-technologies.ch>
2025-04-18 22:01:54 +0200

    New gocomics version

--- getcomics_RSS.py
+++ getcomics_RSS.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-#importing libraries
+# importing libraries
 from html.parser import HTMLParser
-import urllib.request, urllib.error, urllib.parse  
+import urllib.request, urllib.error, urllib.parse
 import gocomicsScrape
 import re
 import os
@@ -19,43 +19,50 @@ import requests
 import json
 
 
-print ("Starting to scrap!")
+print("Starting to scrap!")
 
 #################################
 # Variables that need to be set #
 #################################
 # Tiny Tiny RSS
-RSS_URL = os.environ['RSS_URL']
-RSS_LOGIN = os.environ['RSS_LOGIN']
-RSS_PASSWORD = os.environ['RSS_PASSWORD']
+RSS_URL = os.environ["RSS_URL"]
+RSS_LOGIN = os.environ["RSS_LOGIN"]
+RSS_PASSWORD = os.environ["RSS_PASSWORD"]
 
 
-#Directory to stored the generated feeds
-XML_FOLDER = os.getenv('XML_FOLDER')
+# Directory to stored the generated feeds
+XML_FOLDER = os.getenv("XML_FOLDER")
 # Where the generated feeds will be served -> must be reachable by the RSS server
-RSS_SCRAPPER_URL = os.getenv('RSS_SCRAPPER_URL')
+RSS_SCRAPPER_URL = os.getenv("RSS_SCRAPPER_URL")
 
 #################################
 #    VARIABLES WITH DEFAULTS    #
 #################################
 
 # New feeds get added to this category
-COMICS_DEFAULT_FEED_CATEGORY_TITLE = os.getenv('COMICS_DEFAULT_FEED_CATEGORY_TITLE', "GoComics")
+COMICS_DEFAULT_FEED_CATEGORY_TITLE = os.getenv(
+    "COMICS_DEFAULT_FEED_CATEGORY_TITLE", "GoComics"
+)
 # Favorite feeds category
-COMICS_FAVORITE_FEED_CATEGORY_TITLE = os.getenv('COMICS_FAVORITE_FEED_CATEGORY_TITLE', "GoComics-always")
-# Ignore feeds in this category 
-COMICS_IGNORE_FEED_CATEGORY_TITLE = os.getenv('COMICS_IGNORE_FEED_CATEGORY_TITLE', "GoComics-ignore")
+COMICS_FAVORITE_FEED_CATEGORY_TITLE = os.getenv(
+    "COMICS_FAVORITE_FEED_CATEGORY_TITLE", "GoComics-always"
+)
+# Ignore feeds in this category
+COMICS_IGNORE_FEED_CATEGORY_TITLE = os.getenv(
+    "COMICS_IGNORE_FEED_CATEGORY_TITLE", "GoComics-ignore"
+)
 
 
+VERBOSE = os.getenv("VERBOSE", False)
+MAX_RETRIES = os.getenv("MAX_RETRIES", 10)
+RETRIES_WAIT = os.getenv("RETRIES_WAIT", 5)  # In seconds
 
-VERBOSE = os.getenv('VERBOSE', False)
-MAX_RETRIES = os.getenv('MAX_RETRIES', 10)
-RETRIES_WAIT=os.getenv('RETRIES_WAIT', 5) #In seconds
-
-#Go Comics pages to scrap
-URLS_TO_SCRAP=['http://www.gocomics.com/comics/a-to-z']
-USER_AGENT = os.getenv('USER_AGENT', 'Mozilla/5.0 (compatible; Feedbot/1.0)')
-
+# Go Comics pages to scrap
+URLS_TO_SCRAP = ["https://www.gocomics.com/comics/a-to-z"]
+USER_AGENT = os.getenv(
+    "USER_AGENT",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+)
 
 
 def json_rest_post(input_d):
@@ -64,119 +71,165 @@ def json_rest_post(input_d):
         print("Calling:")
         print(data_json)
     response = requests.post(RSS_URL, data=data_json)
-    #print response.text
-    result=json.loads(response.text)
+    # print response.text
+    result = json.loads(response.text)
     if VERBOSE:
         print((json.dumps(result, indent=4, sort_keys=True)))
-    return result	
+    return result
+
 
 if not os.path.isdir(XML_FOLDER):
     os.mkdir(XML_FOLDER)
 
-# Log into RSS    
-session_id = json_rest_post({"op":"login","user":RSS_LOGIN,"password":RSS_PASSWORD})["content"]["session_id"]
-rss_categories = json_rest_post({"sid":session_id,"op":"getCategories"})["content"]
+# Log into RSS
+session_id = json_rest_post(
+    {"op": "login", "user": RSS_LOGIN, "password": RSS_PASSWORD}
+)["content"]["session_id"]
+rss_categories = json_rest_post({"sid": session_id, "op": "getCategories"})["content"]
+
 
 # Fetch feeds for categories
-def get_feeds_for_category (category_title):
-  comics_category = [x for x in rss_categories if x["title"] == category_title][0]
-  comics_category_id = int(comics_category["id"])    
-  return (comics_category_id, json_rest_post({"sid":session_id, "op":"getFeeds", "cat_id": comics_category_id})["content"])
+def get_feeds_for_category(category_title):
+    comics_category = [x for x in rss_categories if x["title"] == category_title][0]
+    comics_category_id = int(comics_category["id"])
+    return (
+        comics_category_id,
+        json_rest_post(
+            {"sid": session_id, "op": "getFeeds", "cat_id": comics_category_id}
+        )["content"],
+    )
 
 
-comics_default_feeds_id,  comics_default_feeds  = get_feeds_for_category(COMICS_DEFAULT_FEED_CATEGORY_TITLE)
-comics_favorite_feeds_id, comics_favorite_feeds = get_feeds_for_category(COMICS_FAVORITE_FEED_CATEGORY_TITLE)
-comics_ignore_feeds_id,   comics_ignore_feeds   = get_feeds_for_category(COMICS_IGNORE_FEED_CATEGORY_TITLE)
+comics_default_feeds_id, comics_default_feeds = get_feeds_for_category(
+    COMICS_DEFAULT_FEED_CATEGORY_TITLE
+)
+comics_favorite_feeds_id, comics_favorite_feeds = get_feeds_for_category(
+    COMICS_FAVORITE_FEED_CATEGORY_TITLE
+)
+comics_ignore_feeds_id, comics_ignore_feeds = get_feeds_for_category(
+    COMICS_IGNORE_FEED_CATEGORY_TITLE
+)
 
 # Create dictionary of urls
 comics_by_url = {}
+
+
 def add_urls(feeds, is_ignored=False):
-  for feed in feeds:
-    feed_url = feed["feed_url"]
-    comics_by_url[feed_url] = is_ignored
-    
+    for feed in feeds:
+        feed_url = feed["feed_url"]
+        comics_by_url[feed_url] = is_ignored
+
+
 add_urls(comics_default_feeds)
 add_urls(comics_favorite_feeds)
 add_urls(comics_ignore_feeds, is_ignored=True)
 
-#Parse page
+
+# Parse page
 class HomepageParser(HTMLParser):
-  def __init__(self, url):
-    HTMLParser.__init__(self)
-    
-    # Open URL
-    req = urllib.request.Request(url)
-    req.add_header('User-Agent', USER_AGENT)
-    urlopener = urllib.request.urlopen(req)
-    
-    # Parse URL
-    self.__comic_ids = []     
-    self.feed(str(urlopener.read()))
-    
-    # Close parser and URL
-    self.close()
-    urlopener.close()
-    
-    # Check that we found links
-    if len(self.get_comic_ids()) == 0:
-      raise NameError("No links found")
+    def __init__(self, url):
+        HTMLParser.__init__(self)
+        self.in_script = False
+        # Open URL
+        req = urllib.request.Request(url)
+        req.add_header("User-Agent", USER_AGENT)
+        urlopener = urllib.request.urlopen(req)
 
-  
-  def get_comic_ids(self):
-    return self.__comic_ids
+        # Parse URL
+        self.__comic_ids = []
+        self.feed(str(urlopener.read()))
 
-  def handle_starttag(self, tag, attrs):
-    if tag.lower() == 'a':
-      for attr in attrs:
-        if attr[0]=='href':
-          href = attr[1]
-          # Search for comid_id + date. Example: /the-comic-strip-that-has-a-finale-every-day/2019/12/25
-          matchObj = re.match("/(.+?) */\d+/\d+/\d+", href)
-          if matchObj:
-            comic_id = matchObj.group(1)
-            self.__comic_ids.append(comic_id)
+        # Close parser and URL
+        self.close()
+        urlopener.close()
+
+        # Check that we found links
+        if len(self.get_comic_ids()) == 0:
+            raise NameError("No links found")
+
+    def get_comic_ids(self):
+        return self.__comic_ids
+
+    def handle_starttag(self, tag, attrs):
+        if tag.lower() == "script":
+
+            for attr in attrs:
+
+                if attr[0] == "type" and attr[1] == "application/ld+json":
+                    self.in_script = True
+            #         href = attr[1]
+            #         # Search for comid_id + date. Example: /the-comic-strip-that-has-a-finale-every-day/2019/12/25
+            #         matchObj = re.match("/(.+?) */\d+/\d+/\d+", href)
+            #         if matchObj:
+            #             comic_id = matchObj.group(1)
+            #             self.__comic_ids.append(comic_id)
+
+    def handle_endtag(self, tag):
+        if tag.lower() == "script":
+            self.in_script = False
+
+    def handle_data(self, data):
+        if self.in_script:
+            data = data.encode("utf-8").decode("unicode_escape")
+            data = json.loads(data)
+            if "@type" in data.keys() and data["@type"] == "ItemList":
+                # print(data)
+                for item in data["itemListElement"]:
+                    url = item["url"]  # example: 'https://www.gocomics.com/1-and-done'
+                    matchObj = re.match(
+                        "https://www.gocomics.com/(.*)", url
+                    )  # extract the comic id
+                    if matchObj:
+                        comic_id = matchObj.group(1)
+                        self.__comic_ids.append(comic_id)
+
 
 for url_to_scrap in URLS_TO_SCRAP:
-  
-  comic_ids = None
-  for retry in range(MAX_RETRIES):
-    try:
-      parser = HomepageParser(url_to_scrap)
-      comic_ids = parser.get_comic_ids()
-      break;
-    except NameError as e:
-      print(e)
-    except: 
-      print(("Unknown problem: "+str(url_to_scrap)))
-      print((sys.exc_info()[0]), flush=True)
-      
-    print("Retry in %s seconds" % (RETRIES_WAIT))
-    time.sleep(RETRIES_WAIT)
 
-  if comic_ids == None:
-    raise NameError("No more retries for %s" % (url_to_scrap))
-    
-  for comic_id in comic_ids:
-    print('')
-    feed_path = comic_id+'.xml'
-    feed_file = XML_FOLDER+feed_path
-    feed_url = RSS_SCRAPPER_URL+"/"+feed_path
-    
-    if feed_url in comics_by_url:
-      #Already known comic
-      print("* %s found!" % (comic_id))
-      if comics_by_url[feed_url]:
-        #Ignored
-        print('  - continue (ignore)', flush=True)
-        continue
-    else:
-      #Adding new comic
-      print(("Adding "+comic_id), flush=True)
-      json_rest_post({"sid":session_id,"op":"subscribeToFeed", "category_id": comics_default_feeds_id, "feed_url": feed_url})
+    comic_ids = None
+    for retry in range(MAX_RETRIES):
+        try:
+            parser = HomepageParser(url_to_scrap)
+            comic_ids = parser.get_comic_ids()
+            break
+        except NameError as e:
+            print(e)
+        except:
+            print(("Unknown problem: " + str(url_to_scrap)))
+            print((sys.exc_info()[0]), flush=True)
 
-    print(("  - updating feed"), flush=True)
-    xmlContent = gocomicsScrape.scrape(comic_id)
-    with open(feed_file,'w') as xmlfile:
-        xmlfile.write(xmlContent)
+        print("Retry in %s seconds" % (RETRIES_WAIT))
+        time.sleep(RETRIES_WAIT)
 
+    if comic_ids == None:
+        raise NameError("No more retries for %s" % (url_to_scrap))
 
+    for comic_id in comic_ids:
+        print("")
+        feed_path = comic_id + ".xml"
+        feed_file = XML_FOLDER + feed_path
+        feed_url = RSS_SCRAPPER_URL + "/" + feed_path
+
+        if feed_url in comics_by_url:
+            # Already known comic
+            print("* %s found!" % (comic_id))
+            if comics_by_url[feed_url]:
+                # Ignored
+                print("  - continue (ignore)", flush=True)
+                continue
+        else:
+            # Adding new comic
+            print(("Adding " + comic_id), flush=True)
+            json_rest_post(
+                {
+                    "sid": session_id,
+                    "op": "subscribeToFeed",
+                    "category_id": comics_default_feeds_id,
+                    "feed_url": feed_url,
+                }
+            )
+
+        print(("  - updating feed"), flush=True)
+        xmlContent = gocomicsScrape.scrape(comic_id)
+        with open(feed_file, "w") as xmlfile:
+            xmlfile.write(xmlContent)
--- gocomicsScrape.py
+++ gocomicsScrape.py
@@ -7,150 +7,175 @@ import sys
 import time
 import urllib.request, urllib.parse, urllib.error
 import xml.dom.minidom
+import json
 from xml.sax.saxutils import escape as xml_escape
 
-XHTML_NS = 'http://www.w3.org/1999/xhtml'
+XHTML_NS = "http://www.w3.org/1999/xhtml"
 
-numberOfDaysToScrap=7
-SLEEP_BETWEEN_COMICS=10
-SLEEP_BETWEEN_RETRIES=600
-MAX_RETRIES=200
-    
+numberOfDaysToScrap = 7
+SLEEP_BETWEEN_COMICS = 10
+SLEEP_BETWEEN_RETRIES = 600
+MAX_RETRIES = 200
+
+
 def open_url(url):
-  version = 'Mozilla/5.0 (compatible; Feedbot/1.0)'
-  
-  req = urllib.request.Request(url)
-  req.add_header('User-Agent', version)
-  return urllib.request.urlopen(req)
+    version = "Mozilla/5.0 (compatible; Feedbot/1.0)"
+
+    req = urllib.request.Request(url)
+    req.add_header("User-Agent", version)
+    return urllib.request.urlopen(req)
 
 
 def get_homepage_data(strip_id):
-  class HomepageParser(HTMLParser):
-    def __init__(self):
-      HTMLParser.__init__(self)
-      self.title = ''
-      self.in_title = False
+    class HomepageParser(HTMLParser):
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.title = ""
+            self.in_title = False
 
-    def handle_starttag(self, tag, attrs):
-      if tag == "title":
-        self.in_title = True
+        def handle_starttag(self, tag, attrs):
+            if tag == "title":
+                self.in_title = True
 
-    def handle_endtag(self, tag):
-      if tag == "title":
-        self.in_title = False
+        def handle_endtag(self, tag):
+            if tag == "title":
+                self.in_title = False
 
-    def handle_data(self, data):
-      if self.in_title:
-        self.title += data
-        self.title = re.sub(r"\s*\|.*$", "", self.title)
+        def handle_data(self, data):
+            if self.in_title:
+                self.title += data
+                self.title = re.sub(r"\s*\|.*$", "", self.title)
 
-  homepage_url = 'https://www.gocomics.com/%s' % strip_id
-  homepage_file = open_url(homepage_url)
-  parser = HomepageParser()
-  parser.feed(str(homepage_file.read()))
-  parser.close()
-  homepage_file.close()
+    homepage_url = "https://www.gocomics.com/%s" % strip_id
+    homepage_file = open_url(homepage_url)
+    parser = HomepageParser()
+    parser.feed(str(homepage_file.read()))
+    parser.close()
+    homepage_file.close()
 
-  if not parser.title:
-    return None, []
+    if not parser.title:
+        return None, []
 
-  today = datetime.date.today()
-  strips = []
-  for i in range(0, numberOfDaysToScrap):
-    strip_date = today - datetime.timedelta(days=i)
-    strip_url = '%s/%s' % (homepage_url, strip_date.strftime('%Y/%m/%d'))
-    strips.append((strip_date, strip_url))
+    today = datetime.date.today()
+    strips = []
+    for i in range(0, numberOfDaysToScrap):
+        strip_date = today - datetime.timedelta(days=i)
+        strip_url = "%s/%s" % (homepage_url, strip_date.strftime("%Y/%m/%d"))
+        strips.append((strip_date, strip_url))
 
-  return parser.title, strips
+    return parser.title, strips
 
 
 def get_strip_image_url(strip_url):
-  class ImageParser(HTMLParser):
-    def __init__(self):
-      HTMLParser.__init__(self)
-      self.image_url = None
-      self.in_comic_picture = False
+    class ImageParser(HTMLParser):
+        def __init__(self):
+            HTMLParser.__init__(self)
+            self.image_url = None
+            self.in_script = False
+            self.in_viewer = False
 
-    def handle_starttag(self, tag, attrs):
-      if (tag == "picture") and ('class', 'item-comic-image') in attrs:
-        self.in_comic_picture = True
-	
-      # Modernized gocomics.com strips with a <picture> element
-      elif (self.in_comic_picture) and (tag == "img"):
-        if not self.image_url:
-          self.image_url = dict(attrs).get('src', None)
-        return
+        def handle_starttag(self, tag, attrs):
+            if tag.lower() == "section":
+                for attr in attrs:
+                    if attr[0] == "class" and "ShowComicViewer" in attr[1]:
+                        self.in_viewer = True
+            if self.in_viewer and tag.lower() == "script":
+                for attr in attrs:
+                    if attr[0] == "type" and attr[1] == "application/ld+json":
+                        self.in_script = True
 
-    def handle_endtag(self, tag):
-      if tag == "picture":
-        self.in_comic_picture = False
+        def handle_endtag(self, tag):
+            if tag.lower() == "script":
+                self.in_script = False
+            if tag.lower() == "section":
+                self.in_viewer = False
 
-  parser = ImageParser()
-  try:
-    strip_file = open_url(strip_url)
-  except urllib.error.HTTPError as e:
-    if e.code == 302:
-        # Skip over strip URLs that generate redirects, they must not have
-        # existed.
-        return None
-    else:
-        raise
-  parser.feed(str(strip_file.read()))
-  parser.close()
-  strip_file.close()
+        def handle_data(self, data):
+            if self.in_script:
+                data = data.encode("utf-8").decode("unicode_escape")
+                data = json.loads(data)
+                if "@type" in data.keys() and data["@type"] == "ImageObject":
+                    print(data)
+                    self.image_url = data["url"]
 
-  return parser.image_url
+    parser = ImageParser()
+    try:
+        strip_file = open_url(strip_url)
+    except urllib.error.HTTPError as e:
+        if e.code == 302:
+            # Skip over strip URLs that generate redirects, they must not have
+            # existed.
+            return None
+        else:
+            raise
+    parser.feed(str(strip_file.read()))
+    parser.close()
+    strip_file.close()
+
+    return parser.image_url
+
 
 def scrape(comic_id):
 
-  title, strips = get_homepage_data(comic_id)
+    title, strips = get_homepage_data(comic_id)
 
-  xmlContent=[]
-  xmlContent.append('<?xml version="1.0" encoding="utf-8"?>')
-  xmlContent.append('<feed xmlns="http://www.w3.org/2005/Atom">')
-  xmlContent.append('<title>%s</title>' % xml_escape(title))
+    xmlContent = []
+    xmlContent.append('<?xml version="1.0" encoding="utf-8"?>')
+    xmlContent.append('<feed xmlns="http://www.w3.org/2005/Atom">')
+    xmlContent.append("<title>%s</title>" % xml_escape(title))
 
-  strip_count = 0
-  for strip_date, strip_url in strips:	  
-    counter=0
-    #print time.localtime()
-    while True:  
-      try:  
-        strip_image_url = get_strip_image_url(strip_url)
-        break
-      except :
-        print ("Retrying %s in %d" % (strip_url, SLEEP_BETWEEN_RETRIES), file=sys.stderr)
-        counter=counter+1
-        if counter >=MAX_RETRIES:
-          break
-        time.sleep(SLEEP_BETWEEN_RETRIES)
-    if not strip_image_url:
-      continue
-    strip_count += 1
-    xmlContent.append('<entry>')
-    xmlContent.append('  <title>%s</title>' % xml_escape(strip_date.strftime('%A, %B %d, %Y')))
-    xmlContent.append('  <id>%s</id>' % strip_url)
-    xmlContent.append('  <published>%sT12:00:00.000Z</published>' % strip_date.isoformat())
-    xmlContent.append('  <link rel="alternate" href="%s" type="text/html"/>' % xml_escape(strip_url))
-    xmlContent.append('  <content type="xhtml">')
-    xmlContent.append('    <div xmlns="%s"><img src="%s"/></div>' % (XHTML_NS, xml_escape(strip_image_url)))
-    xmlContent.append('  </content>')
-    xmlContent.append('</entry>')
-    
-    time.sleep(SLEEP_BETWEEN_COMICS)
-    
+    strip_count = 0
+    for strip_date, strip_url in strips:
+        counter = 0
+        # print time.localtime()
+        while True:
+            try:
+                strip_image_url = get_strip_image_url(strip_url)
+                break
+            except:
+                print(
+                    "Retrying %s in %d" % (strip_url, SLEEP_BETWEEN_RETRIES),
+                    file=sys.stderr,
+                )
+                counter = counter + 1
+                if counter >= MAX_RETRIES:
+                    break
+                time.sleep(SLEEP_BETWEEN_RETRIES)
+        if not strip_image_url:
+            continue
+        strip_count += 1
+        xmlContent.append("<entry>")
+        xmlContent.append(
+            "  <title>%s</title>" % xml_escape(strip_date.strftime("%A, %B %d, %Y"))
+        )
+        xmlContent.append("  <id>%s</id>" % strip_url)
+        xmlContent.append(
+            "  <published>%sT12:00:00.000Z</published>" % strip_date.isoformat()
+        )
+        xmlContent.append(
+            '  <link rel="alternate" href="%s" type="text/html"/>'
+            % xml_escape(strip_url)
+        )
+        xmlContent.append('  <content type="xhtml">')
+        xmlContent.append(
+            '    <div xmlns="%s"><img src="%s"/></div>'
+            % (XHTML_NS, xml_escape(strip_image_url))
+        )
+        xmlContent.append("  </content>")
+        xmlContent.append("</entry>")
 
-  #if not strip_count:
-    #xmlContent.append '<entry>'
-    #xmlContent.append '  <title>Could not scrape feed</title>'
-    #xmlContent.append '  <id>tag:persistent.info,2013:gocomics-scrape-%d</id>' % int(time.time())
-    #xmlContent.append '  <link rel="alternate" href="https://github.com/mihaip/feed-scraping" type="text/html"/>'
-    #xmlContent.append '  <content type="html">'
-    #xmlContent.append '    Could not scrape the feed. Check the GitHub repository for updates.'
-    #xmlContent.append '  </content>'
-    #xmlContent.append '</entry>'
+        time.sleep(SLEEP_BETWEEN_COMICS)
 
+    # if not strip_count:
+    # xmlContent.append '<entry>'
+    # xmlContent.append '  <title>Could not scrape feed</title>'
+    # xmlContent.append '  <id>tag:persistent.info,2013:gocomics-scrape-%d</id>' % int(time.time())
+    # xmlContent.append '  <link rel="alternate" href="https://github.com/mihaip/feed-scraping" type="text/html"/>'
+    # xmlContent.append '  <content type="html">'
+    # xmlContent.append '    Could not scrape the feed. Check the GitHub repository for updates.'
+    # xmlContent.append '  </content>'
+    # xmlContent.append '</entry>'
 
-  xmlContent.append('</feed>')
-  
-  return "\n".join(xmlContent)
+    xmlContent.append("</feed>")
+
+    return "\n".join(xmlContent)
